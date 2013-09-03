034bd608c80c81945106a0363efce1ea3ef181b9
gyianz <gyianz0@gmail.com>
2013-09-03 22:19:41 +0800

    Initial commit 	new file:   .classpath 	new file:   .project 	new file:   .settings/org.eclipse.jdt.core.prefs 	new file:   bin/newDES.class 	new file:   src/newDES.java

---
+++ src/newDES.java
@@ -0,0 +1,169 @@
+
+public class newDES {
+
+//=================================================SUBKEY GENERATION SECTION===============================================================	
+	
+	/** Initial matrix to reduce the 64 bit key to 56 bits key. Every 8th bit is removed
+	 * 
+	 */
+	private static final byte[] PC1 = {
+        57, 49, 41, 33, 25, 17, 9,
+        1, 58, 50, 42, 34, 26, 18,
+        10, 2, 59, 51, 43, 35, 27,
+        19, 11, 3, 60, 52, 44, 36,
+        63, 55, 47, 39, 31, 23, 15,
+        7, 62, 54, 46, 38, 30, 22,
+        14, 6, 61, 53, 45, 37, 29,
+        21, 13, 5, 28, 20, 12, 4
+	};
+	
+	/** Separate the 56 bits subkey into 2 parts and left shift both of them accordingly
+	 *  Each key rotation is retro active. C15D15 relies on C14D14 and C14D14 relies on C13D13 and so on
+	 */
+    private static final byte[] subkeyShift = {
+        1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1
+    };
+	
+    /** After shifting the keys, we get 16 56-bits subkeys. from here we reduce the 56 bits subkeys to 48 bits
+     *  We do this for all the 16 keys
+     * 
+     */
+	private static final byte[] PC2 = {
+        14, 17, 11, 24, 1, 5,
+        3, 28, 15, 6, 21, 10,
+        23, 19, 12, 4, 26, 8,
+        16, 7, 27, 20, 13, 2,
+        41, 52, 31, 37, 47, 55,
+        30, 40, 51, 45, 33, 48,
+        44, 49, 39, 56, 34, 53,
+        46, 42, 50, 36, 29, 32
+	};
+	
+//=====================================================MESSAGE ENCRYPTION SECTION==========================================================
+	
+	/**
+	 * Take a 64 bit message and permute it with the IP matrix to get a new string of bits which we call it M+
+	 * After that we split it into both L0 and R0 which is 32 bits each
+	 */
+	private static final byte[] IP = {
+        58, 50, 42, 34, 26, 18, 10, 2,
+        60, 52, 44, 36, 28, 20, 12, 4,
+        62, 54, 46, 38, 30, 22, 14, 6,
+        64, 56, 48, 40, 32, 24, 16, 8,
+        57, 49, 41, 33, 25, 17, 9, 1,
+        59, 51, 43, 35, 27, 19, 11, 3,
+        61, 53, 45, 37, 29, 21, 13, 5,
+        63, 55, 47, 39, 31, 23, 15, 7
+	};
+	
+	/**
+	 * From here we going to use this function to encrypt the message
+	 * Ln = R(n-1)
+	 * Rn = L(n-1) + f[R(n-1), Kn].
+	 * Where the + is actually a XOR function
+	 * Example when N = 1
+	 * L1 = R0
+	 * R1 = L0 + f[R0 , K1]
+	 * 
+	 * Starting off with the E bit-selection table where it takes in 32 bit and gives a 48 bit output
+	 * We simply permute this matrix with the R[n-1] bits 
+	 * After that we simply XOR the 48 bits output with the Subkey K[n]
+	 */
+    private static final byte[] E = {
+        32, 1, 2, 3, 4, 5,
+        4, 5, 6, 7, 8, 9,
+        8, 9, 10, 11, 12, 13,
+        12, 13, 14, 15, 16, 17,
+        16, 17, 18, 19, 20, 21,
+        20, 21, 22, 23, 24, 25,
+        24, 25, 26, 27, 28, 29,
+        28, 29, 30, 31, 32, 1
+    };
+    
+    /**
+     * The result of the XOR from the E-bit selection table and the subkey is then divided into 8 parts of B1,B2...B8 of 6 bits each
+     * Each of this parts are then processed through the S-Box.
+     * S1(B1)S2(B2)...S8(B8)
+     * Each part is then broken down into 2 parts
+     * First :
+     *  The first and last bit of B1 is taken and concantenated together to form a binary number that determines which row we should take from
+     *  The middle 4 bits is the one that determines which column we should take from
+     * The number from the respective S-boxes are then converted into binary and concatenated together to form a 32 bit output
+     */
+    
+    private static final byte[][] S = {{
+        14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7,
+        0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8,
+        4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0,
+        15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13
+    }, {
+        15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10,
+        3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5,
+        0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15,
+        13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9
+    }, {
+        10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8,
+        13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1,
+        13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7,
+        1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12
+    }, {
+        7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15,
+        13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9,
+        10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4,
+        3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14
+    }, {
+        2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9,
+        14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6,
+        4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14,
+        11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3
+    }, {
+        12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11,
+        10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8,
+        9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6,
+        4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13
+    }, {
+        4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1,
+        13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6,
+        1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2,
+        6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12
+    }, {
+        13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7,
+        1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2,
+        7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8,
+        2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11
+    }};
+    
+    /**
+     * The result of the function F is then permuted with the P matrix and the result is then XOR with the L[N-1] to give us the
+     * string of bits for the R[N]
+     * 
+     * We then do this for 16 rounds. After getting R16 and L16, they are both concatenated in such a way : R16L16
+     */
+    private static final byte[] P = {
+        16, 7, 20, 21,
+        29, 12, 28, 17,
+        1, 15, 23, 26,
+        5, 18, 31, 10,
+        2, 8, 24, 14,
+        32, 27, 3, 9,
+        19, 13, 30, 6,
+        22, 11, 4, 25
+    };
+    
+    /**
+     * After getting the concatenation of R16L16, we then permute it with the FP matrix to produce us the final encrypted form of
+     * the message
+     */
+    private static final byte[] FP = {
+        58, 50, 42, 34, 26, 18, 10, 2,
+        60, 52, 44, 36, 28, 20, 12, 4,
+        62, 54, 46, 38, 30, 22, 14, 6,
+        64, 56, 48, 40, 32, 24, 16, 8,
+        57, 49, 41, 33, 25, 17, 9, 1,
+        59, 51, 43, 35, 27, 19, 11, 3,
+        61, 53, 45, 37, 29, 21, 13, 5,
+        63, 55, 47, 39, 31, 23, 15, 7
+    };
+
+
+}
